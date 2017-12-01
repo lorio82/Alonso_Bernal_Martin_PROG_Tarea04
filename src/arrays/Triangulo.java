6b472bb686d9bc35b3570903dd1e8ca302f4785d
malon <malon@DESKTOP-FC0QCP3>
2017-12-01 20:50:42 +0100

    Creamos la clase Triangulo y declaramos sus atributos

---
+++ src/arrays/Triangulo.java
@@ -0,0 +1,16 @@
+
+package arrays;
+
+/**
+ * Clase Triángulo que crea un objeto triángulo definido por sus tres vértices
+ * (A,B y C).
+ *
+ * @author: Martín Alonso Bernal
+ * @version: 11/2017
+ */
+public class Triangulo {
+    private Punto verticeA;
+    private Punto verticeB;
+    private Punto verticeC;
+    
+}
