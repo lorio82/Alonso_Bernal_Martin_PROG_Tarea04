025245e40faeda7a4bdeb218bc1092c4b0613108
malon <malon@DESKTOP-FC0QCP3>
2017-12-01 22:18:27 +0100

    Generamos el código necesario para realizar los cálculos solicitados y mostrar el resultado por pantalla.

--- src/arrays/Principal.java
+++ src/arrays/Principal.java
@@ -1,17 +1,19 @@
-
 package arrays;
 
 import utilidades.Entrada;
 
 /**
- * Clase Principal que solicita al usuario la introducción de un número de triángulos a generar, para devolver el 
- * valor de la media de los perímetros de todos los triángulos creados, cuantos triángulos están en la media 
- * o por encima y cuantos por debajo. También devolverá los valores de del triángulo de mayor perímetro y del que menor perímetro tenga.
+ * Clase Principal que solicita al usuario la introducción de un número de
+ * triángulos a generar, para devolver el valor de la media de los perímetros de
+ * todos los triángulos creados, cuantos triángulos están en la media o por
+ * encima y cuantos por debajo. También devolverá los valores de del triángulo
+ * de mayor perímetro y del que menor perímetro tenga.
  *
  * @author: Martín Alonso Bernal
  * @version: 11/2017
  */
 public class Principal {
+
     public static void main(String[] args) {
         final String ENUNCIADO = "Introduzca el número de triángulos que desee crear";
         Punto verticeA;
@@ -19,16 +21,24 @@ public class Principal {
         Punto verticeC;
         double perimetroTotal = 0;
         double media = 0;
-        
+        int contMayor = 0;
+        int contMenor = 0;
+        double comparaMayor = 0;
+        double comparaMenor = 1000;
+        String trianguloMayor = "";
+        String trianguloMenor = "";
+        int numero = 0;
+        int numeroMin = 0;
+
         System.out.printf(ENUNCIADO);
         int num = Entrada.entero();
-        
+
         //Creamos un array de objetos punto formado por el número de triángulos que desamos crear
         Punto[] arrayPunto = new Punto[num];
-        
+
         //Creamos un array de objetos triángulo formado por el número de triángulos que desamos crear
         Triangulo[] arrayTriangulo = new Triangulo[num];
-        
+
         //En este for recorremos el arrayTriangulo
         for (int i = 0; i < arrayTriangulo.length; i++) {
             //Le asignamos a cada uno de los vértices una instancia de punto
@@ -43,10 +53,50 @@ public class Principal {
             media = perimetroTotal / num;
             /* Con la siguiente línea vamos imprimiendo los valores de cada uno de los triángulos
             System.out.printf(i + 1 + arrayTriangulo[i].toString() + "Perimetro: " + arrayTriangulo[i].getPerimetro() + "%n");
-            */
-           
+             */
+
         }
-    
+        //En este for calculamos cuantos triángulos tienen un perimtro igual o mayor que la media y cuantos tienen un perímetro menor.
+        for (int i = 0; i < arrayTriangulo.length; i++) {
+
+            if (media < arrayTriangulo[i].getPerimetro()) {
+                contMayor++;
+            } else {
+                contMenor++;
+            }
+            //Con este if comparamos cada uno de los perímetros para quedarnos con el perímetro de mayor tamaño 
+            if (arrayTriangulo[i].getPerimetro() >= comparaMayor) {
+                comparaMayor = arrayTriangulo[i].getPerimetro();
+            }
+        }
+        //Con este for recorremos de nuevo el array de triángulos para quedarnos con el perímetro de menor tamaño
+        for (int i = 0; i < arrayTriangulo.length; i++) {
+            if (arrayTriangulo[i].getPerimetro() < comparaMenor) {
+                comparaMenor = arrayTriangulo[i].getPerimetro();
+            }
+        }
+        //Recorremos de nuevo el array de triángulos para quedarnos con el triángulo cuyo perímetro es igual al de mayor tamaño.
+        for (int i = 0; i < arrayTriangulo.length; i++) {
+            if (arrayTriangulo[i].getPerimetro() == comparaMayor) {
+                trianguloMayor = arrayTriangulo[i].toString();
+                numero = i + 1;
+            }
+        }
+        //Recorremos de nuevo el array de triángulos para quedarnos con el triángulo cuyo perímetro es igual al de menor tamaño.
+        for (int i = 0; i < arrayTriangulo.length; i++) {
+
+            if (arrayTriangulo[i].getPerimetro() == comparaMenor) {
+                trianguloMenor = arrayTriangulo[i].toString();
+                numeroMin = i + 1;
+            }
+        }
+
+        System.out.printf("La suma de todos los perímetros es %f. Y la media de los perimetros es%f%n"
+                + "Hay %d triángulo/s con un perímetro superior o igual a la media.%nHay %d triángulo/s con "
+                + "un perímetro menor a la media."
+                + "%nEl triángulo con el perímetro mayor es el número %d%n%s"
+                + "%nEl triángulo con el perímetro menor es el número %d%n%s",
+                perimetroTotal, media, contMayor, contMenor, numero, trianguloMayor, numeroMin, trianguloMenor);
     }
-    
+
 }
