5d2c85947d78a32fec9255cf345cef4391e7949d
malon <malon@DESKTOP-FC0QCP3>
2017-12-01 22:06:44 +0100

    Creamos el código necesario para código necesario para pedir cuántos triángulos queremos generar y crear un array de Puntos de dicho tamaño

--- src/arrays/Principal.java
+++ src/arrays/Principal.java
@@ -5,12 +5,28 @@
  */
 package arrays;
 
+import utilidades.Entrada;
+
 /**
+ * Clase Principal que solicita al usuario la introducción de un número de triángulos a generar, para devolver el 
+ * valor de la media de los perímetros de todos los triángulos creados, cuantos triángulos están en la media 
+ * o por encima y cuantos por debajo. También devolverá los valores de del triángulo de mayor perímetro y del que menor perímetro tenga.
  *
- * @author malon
+ * @author: Martín Alonso Bernal
+ * @version: 11/2017
  */
 public class Principal {
     public static void main(String[] args) {
+        final String ENUNCIADO = "Introduzca el número de triángulos que desee crear";
+        
+        System.out.printf(ENUNCIADO);
+        int num = Entrada.entero();
+        
+        //Creamos un array de objetos punto formado por el número de triángulos que desamos crear
+        Punto[] arrayPunto = new Punto[num];
+        
+        //Creamos un array de objetos triángulo formado por el número de triángulos que desamos crear
+        Triangulo[] arrayTriangulo = new Triangulo[num];
     
     }
     
