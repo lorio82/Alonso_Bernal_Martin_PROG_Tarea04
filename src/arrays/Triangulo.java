7e3ca5108b09ff6b54ee49d05bf24bfabc6f9444
malon <malon@DESKTOP-FC0QCP3>
2017-12-01 20:52:12 +0100

    Creamos el método constructor de la clase Triangulo.

--- src/arrays/Triangulo.java
+++ src/arrays/Triangulo.java
@@ -12,5 +12,22 @@ public class Triangulo {
     private Punto verticeA;
     private Punto verticeB;
     private Punto verticeC;
+    private double x;
+    private double y;
+    
+    /**
+     * Método constructor Triangulo que recibe los parámetros verticeA, verticeB
+     * y verticeC y les asigna como valor a cada uno de ellos una ocurrencia de
+     * la clase Punto
+     *
+     * @param verticeA Punto
+     * @param verticeB Punto
+     * @param verticeC Punto
+     */
+    public Triangulo(Punto verticeA, Punto verticeB, Punto verticeC) {
+        this.verticeA = new Punto(x, y);
+        this.verticeB = new Punto(x, y);
+        this.verticeC = new Punto(x, y);
+    }
     
 }
