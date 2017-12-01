325d14f853dfca07234c574f65a38cc8c8d3c129
malon <malon@DESKTOP-FC0QCP3>
2017-12-01 20:43:28 +0100

    Creamos el constructor de la clase Punto.

--- src/arrays/Punto.java
+++ src/arrays/Punto.java
@@ -1,16 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package arrays;
 
 /**
+ * Clase Punto que crea un objeto punto definido por unas coordenadas x,y.
  *
- * @author malon
+ * @author: Martín Alonso Bernal
+ * @version: 11/2017
  */
 public class Punto {
     private double x;
     private double y;
+    
+    /**
+     * Método constructor Punto que recibe los parámetros x e y, y les asigna un número aleatorio entre 1 y 100
+     * @param x double
+     * @param y double
+    */
+    public Punto(double x, double y) {
+        this.x = Math.random()*100;
+        this.y = Math.random()*100;
+    }
     
 }
